a9a865bfba08c0d6e1891df6fa838fd76d19fc52
Jeff Hemminger <jeff@atware.co.jp>
2015-07-03 15:13:57 +0900

    added multiple cursors, a few other nice purcell features

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("118717ce0a2645a0cf240b044999f964577ee10137b1f992b09a317d5073c02d" default)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
